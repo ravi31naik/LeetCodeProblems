79827fbd9004830c07124bfa845e3bd3b47f033c
Ravi Naik <@RAVILAPTOP>
2018-01-24 17:58:03 -0800

    TwoSum

---
+++ TwoSum/TwoNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 230, 863, 916, 585, 981, 404, 316, 785, 88, 12, 70, 435, 384, 778, 887, 755, 740, 337, 86, 92, 325, 422, 815, 650, 920, 125, 277, 336, 221, 847, 168, 23, 677, 61, 400, 136, 874, 363, 394, 199, 863, 997, 794, 587, 124, 321, 212, 957, 764, 173, 314, 422, 927, 783, 930, 282, 306, 506, 44, 926, 691, 568, 68, 730, 933, 737, 531, 180, 414, 751, 28, 546, 60, 371, 493, 370, 527, 387, 43, 541, 13, 457, 328, 227, 652, 365, 430, 803, 59, 858, 538, 427, 583, 368, 375, 173, 809, 896, 370, 789 };
+            int target = 542;
+            int[] ans = TwoSumDictonary(nums, target);
+            Console.WriteLine(ans[0]);
+            Console.WriteLine(ans[1]);
+            Console.ReadLine();
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] temp1 = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = (nums.Length - 1); j >= i + 1; j--)
+                {
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        temp1[0] = i;
+                        temp1[1] = j;
+                        return temp1;
+                    }
+                }
+            }
+            return temp1;
+        }
+
+        public static int[] TwoSumDictonary(int[] nums, int target)
+        {
+            Dictionary<int, int> myDictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complimant = target - nums[i];
+                if (myDictionary.ContainsKey(complimant))
+                {
+                    return new int[] { myDictionary[complimant], i };
+                }
+                myDictionary.TryAdd(nums[i], i);
+            }
+            throw new Exception("Exception");
+        }
+    }
+}
